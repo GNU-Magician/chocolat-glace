ba0ab46eb837c72bb1b4410181ff19463d3d544e
GNU-Magician <31811114+gnu-magician@users.noreply.github.com>
2018-12-09 21:19:16 -0200

    added all crud actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,24 +10,45 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
 
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Post was made successfully!"
+      redirect_to posts_path
+    else
+      render "new"
+    end
 
   end
 
   def edit
+    @post = Post.find(params[:id])
 
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:success] = "You succesfully deleted the post."
+    redirect_to posts_path
   end
 
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 
 end
